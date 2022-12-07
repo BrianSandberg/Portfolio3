768cad4d5994f94fbe0f935946041df876ed30d3
Mads jonsson <madsjonsson1997@gmail.com>
2022-12-07 15:11:54 +0100

    Someth8ing

--- src/App.js
+++ src/App.js
@@ -105,23 +105,26 @@ lol remove this line, this is part of a test
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 import Landing from "./pages/Landing";
-import "bootstrap/dist/css/bootstrap.min.css"
+import TitlesSearchResult from "./Components/Titles/TitlesSearchResult/TitlesSearchResult";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="*" element={<NoPage />} />
           <Route index element={<Landing />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="searchResults" element={<TitlesSearchResult />} /> 
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/Components/Navigation/NavigationBar.jsx
@@ -0,0 +1,44 @@
+import { Outlet, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import TitlesSearchResult from "../Titles/TitlesSearchResult/TitlesSearchResult";
+
+const NavigationBar = () => {
+    const [searchType, setSearchType] = useState("titles");
+
+function selectSearchType(type) {
+    setSearchType(type);
+    console.log(type);
+}
+
+function goToResultsPage(searchTerm) {
+    <Link to="searchResults"></Link>
+    console.log(searchTerm);
+
+}
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>      
+      <input id="SearchField" type="text" placeholder="Search here" />
+      <input type="radio" name="SearchType" id="SearchTitles" value="Titles" onClick={() => selectSearchType("titles")}/> 
+      <label for="SearchTitles">Titles</label>
+      <input type="radio" name="SearchType" id="SearchPersons" value="Persons" onClick={() => selectSearchType("persons")}/> 
+      <label for="SearchPersons">Persons</label>
+     {  <button id="SearchButton" onClick={() => goToResultsPage(document.getElementById("SearchField").value)}>Search</button> }
+    </>
+  )
+};
+
+export default NavigationBar;
