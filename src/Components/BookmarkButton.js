bca66d5fb73dadad93a9c92cfb05174a0d11a21b
BrianSandberg <oow@ruc.dk>
2022-12-15 16:42:35 +0100

    Button works

--- src/Components/BookmarkButton.js
+++ src/Components/BookmarkButton.js
@@ -4,29 +4,51 @@ import React, { useState, useEffect } from 'react';
 //Skal lave to forskellige buttons, så der både er til actors og titles
 function CheckObjectButton() {
   const [objectExists, setObjectExists] = useState(false);
+  //const [bookmarkList, setbookmarkList] = useState([]);
+
+  const apiBase = "http://localhost:5001/api/users/"
+  //Get username from variable
+  const username = "testing123";
+  //Get titleID from variable from search
+  const titleTest = "tt0052520"
   //const objectExists = false;
   console.log("asdf")
 
   useEffect(() => {
     const fetchObject = async () => {
-      const response = await fetch('http://localhost:5001/api/users/testing123');
+      const response = await fetch(apiBase + username);
       console.log(response.status)
+
       if (response.status === 200) {
         const data = await response.json();
-        setObjectExists(true);
+        //setbookmarkList(data.bookmarkedTitles);
+        console.log(data.bookmarkedTitles);
 
-        console.log("data: ", data)
-        console.log("ex: ", objectExists)
+        data.bookmarkedTitles.map((bookmark) => {
+          console.log("penis" + bookmark.title_ID);
+          console.log(titleTest);
+          //const tempString = bookmark.title_ID;
+
+          if (bookmark.title_ID == titleTest){
+            setObjectExists(true);
+            console.log("print");
+          }
+          
+        }
+        )
+        console.log(data.bookmarkedTitles[0].title_ID)
       }
+      
     };
     fetchObject();
-  });
+    //[] Forhindre programmet i at render hele tiden.. Idk how
+  }, []);
 
 
   return (
     <button
       style={{
-        backgroundColor: objectExists ? 'white' : 'blue',
+        backgroundColor: objectExists ? 'blue' : 'white',
       }}
     >
       Check if object exists
