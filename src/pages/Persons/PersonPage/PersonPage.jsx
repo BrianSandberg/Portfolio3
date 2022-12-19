60e44d97bce70f4241ce4a8826c4b4968aa07bdf
BrianSandberg <oow@ruc.dk>
2022-12-19 17:58:46 +0100

    Bookmarkperson button

--- src/pages/Persons/PersonPage/PersonPage.jsx
+++ src/pages/Persons/PersonPage/PersonPage.jsx
@@ -58,6 +58,7 @@ function PersonPage() {
                                 <tr><td><p><b>Birth Year: </b>{personElements.birthYear}</p></td></tr>
                                 <tr><td><p><b>Death Year: </b>{personElements.deathYear}</p></td></tr>
                                 <tr><td><p><b>Co-Actors: </b><button onClick={() => navigate("/person/" + id + "/CoActors")}>View Co-Acters</button></p></td></tr>
+                                <tr><td>{BookmarkPersonButton()}</td></tr>
                             </tbody>
                         </table>
                     </Container>
